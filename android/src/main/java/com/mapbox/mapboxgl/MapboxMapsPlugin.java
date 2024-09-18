7776c070b2971a6a108a07d7cf43ee78938ff672
trungnq <trungnguyen@aggregatoricapaci.com>
2024-09-18 22:52:00 +0700

    first commit

--- android/src/main/java/com/mapbox/mapboxgl/MapboxMapsPlugin.java
+++ android/src/main/java/com/mapbox/mapboxgl/MapboxMapsPlugin.java
@@ -2,9 +2,7 @@ package com.mapbox.mapboxgl;
 
 import android.app.Activity;
 import android.app.Application;
-import android.content.Intent;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -12,8 +10,14 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.platform.PlatformViewRegistry;
+import io.flutter.plugin.common.PluginRegistry;
 
+/**
+ * Plugin for controlling a set of MapboxMap views to be shown as overlays on top of the Flutter
+ * view. The overlay should be hidden during transformations or while Flutter is rendering on top of
+ * the map. A Texture drawn using MapboxMap bitmap snapshots can then be shown instead of the
+ * overlay.
+ */
 public class MapboxMapsPlugin implements FlutterPlugin, ActivityAware, Application.ActivityLifecycleCallbacks {
   static final int CREATED = 1;
   static final int STARTED = 2;
@@ -23,77 +27,114 @@ public class MapboxMapsPlugin implements FlutterPlugin, ActivityAware, Applicati
   static final int DESTROYED = 6;
 
   private final AtomicInteger state = new AtomicInteger(0);
+  private int registrarActivityHashCode;
   private MethodChannel methodChannel;
-  private Activity activity;
 
+  // Đăng ký với API cũ
+  public static void registerWith(PluginRegistry.Registrar registrar) {
+    if (registrar.activity() == null) {
+      return; // Không có activity, dừng lại
+    }
+    final MapboxMapsPlugin plugin = new MapboxMapsPlugin();
+    plugin.registrarActivityHashCode = registrar.activity().hashCode();
+    registrar.activity().getApplication().registerActivityLifecycleCallbacks(plugin);
+
+    registrar.platformViewRegistry().registerViewFactory("plugins.flutter.io/mapbox_gl", new MapboxMapFactory(plugin.state, registrar));
+
+    // Khởi tạo MethodChannel
+    plugin.setupChannel(registrar.messenger());
+  }
+
+  // Đăng ký với API mới
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-    methodChannel = new MethodChannel(binding.getBinaryMessenger(), "plugins.flutter.io/mapbox_gl");
-    methodChannel.setMethodCallHandler(new GlobalMethodHandler());
+  public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
+    setupChannel(binding.getBinaryMessenger());
+  }
 
-    // Register the view factory
-    PlatformViewRegistry registry = binding.getPlatformViewRegistry();
-    registry.registerViewFactory("plugins.flutter.io/mapbox_gl", new MapboxMapFactory(state));
+  private void setupChannel(BinaryMessenger messenger) {
+    methodChannel = new MethodChannel(messenger, "plugins.flutter.io/mapbox_gl");
+    methodChannel.setMethodCallHandler(new GlobalMethodHandler());
   }
 
   @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+  public void onDetachedFromEngine(FlutterPlugin.FlutterPluginBinding binding) {
     methodChannel.setMethodCallHandler(null);
   }
 
   @Override
-  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-    this.activity = binding.getActivity();
-    activity.getApplication().registerActivityLifecycleCallbacks(this);
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    this.registrarActivityHashCode = binding.getActivity().hashCode();
+    binding.addActivityResultListener(this);
+    binding.addRequestPermissionsResultListener(this);
+    binding.getLifecycle().addObserver(this);
   }
 
   @Override
   public void onDetachedFromActivityForConfigChanges() {
-    // Not used
+    // Xử lý khi tách activity cho thay đổi cấu hình
   }
 
   @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-    onAttachedToActivity(binding);
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    this.registrarActivityHashCode = binding.getActivity().hashCode();
   }
 
   @Override
   public void onDetachedFromActivity() {
-    activity = null;
+    // Dọn dẹp khi tách activity
   }
 
-  // Activity Lifecycle Callbacks
+  // Các phương thức vòng đời Activity
   @Override
   public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+    if (activity.hashCode() != registrarActivityHashCode) {
+      return;
+    }
     state.set(CREATED);
   }
 
   @Override
   public void onActivityStarted(Activity activity) {
+    if (activity.hashCode() != registrarActivityHashCode) {
+      return;
+    }
     state.set(STARTED);
   }
 
   @Override
-  public void onActivityResumed(Activity activity) {
+  public void onActivityResumed(Activity activity, Bundle savedInstanceState) {
+    if (activity.hashCode() != registrarActivityHashCode) {
+      return;
+    }
     state.set(RESUMED);
   }
 
   @Override
   public void onActivityPaused(Activity activity) {
+    if (activity.hashCode() != registrarActivityHashCode) {
+      return;
+    }
     state.set(PAUSED);
   }
 
   @Override
   public void onActivityStopped(Activity activity) {
+    if (activity.hashCode() != registrarActivityHashCode) {
+      return;
+    }
     state.set(STOPPED);
   }
 
   @Override
   public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+    // Không cần xử lý
   }
 
   @Override
   public void onActivityDestroyed(Activity activity) {
+    if (activity.hashCode() != registrarActivityHashCode) {
+      return;
+    }
     state.set(DESTROYED);
   }
 }
